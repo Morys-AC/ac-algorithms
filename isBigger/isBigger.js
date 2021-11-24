e48f4d83dac08050e4b39b530050e856f2faa758
Morys <Morys@anniecannons.com>
2021-11-24 13:04:24 -0600

    isBigger practice with bonus

--- isBigger/isBigger.js
+++ isBigger/isBigger.js
@@ -5,8 +5,21 @@ return false if a < b, return 0 otherwise.
 Bonus 1: If a or b is undefined or not a number, then return -1.
 Hint: Chekout typeof() operator at
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof
-
 */
+
+function isBigger(a, b) {
+	if (typeof a === "string" || typeof a === undefined || typeof b === "string" || typeof b === undefined) {
+		return -1;
+	}else if (a > b) {
+    return true;
+  }else if (a < b) {
+    return false;
+  }else {
+    return 0;
+  }
+}
+
+console.log(isBigger(10, 8));
 
 /**
 Examples:
