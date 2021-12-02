f96e4b2b1f06cfe0ba53188819fb461fb0f71ebd
Morys <Morys@anniecannons.com>
2021-12-02 11:47:22 -0600

    Last push was a word count, not longest word. This is the proper push.

--- longestWord.js
+++ longestWord.js
@@ -20,7 +20,14 @@
 
 
 function findLongestWord(str) { 
-  return str.split(" ").length;
+  var strSplit = str.split(' ');
+  var longestWord = 0;
+  for(var i = 0; i < strSplit.length; i++){
+    if(strSplit[i].length > longestWord){
+	longestWord = strSplit[i].length;
+     }
+  }
+  return longestWord;
 }
 
 console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
